a04f47af356cb1a084cb4e04b7deb73cc0ceaf10
Robin Saillard <robin.saillard@hotmail.fr>
2022-04-20 15:31:30 +0200

    v1.0.3

--- BarcodeReader/Services/DirectoryWatcher.cs
+++ BarcodeReader/Services/DirectoryWatcher.cs
@@ -1,15 +1,8 @@
 ﻿using RawPrint;
-using Spire.Pdf;
 using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Printing;
 using System.IO;
-using System.Linq;
 using System.Printing;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace BarcodeReader.Services
@@ -21,8 +14,10 @@ namespace BarcodeReader.Services
     {
         public string Directory { get; set; }
         public static string PrinterName { get; set; }
+
         public FileSystemWatcher watcher;
         public static string PathFilePrint { get; set; }
+        public static string DocumentName { get; set; }
         public static string[] ListFileName { get; set; }
 
         public DirectoryWatcher(string dir, string[] filename = null, string ext = "*", string printerName = "Adobe PDF")
@@ -56,54 +51,43 @@ namespace BarcodeReader.Services
                 {
                     System.Threading.Thread.Sleep(1000);
                     PathFilePrint = e.FullPath;
-                    if (File.Exists(PathFilePrint))
-                    {
-                        try
-                        {
-                            //PdfDocument printer = new PdfDocument();
-                            //printer.LoadFromFile(PathFilePrint);
-                            //printer.PrintSettings.PrinterName = PrinterName;
-                            //printer.Print();
-                            //File.Delete(PathFilePrint);
-
-
-                            IPrinter printer = new Printer();
-                            printer.PrintRawFile(PrinterName, PathFilePrint, filename);
-                            
-                            //System.Threading.Thread.Sleep(1000);
-                            //File.Delete(PathFilePrint);
-
-                            var myPrintServer = new LocalPrintServer();
-                            var pq = myPrintServer.GetPrintQueue(PrinterName);
-
-                            var jobs = pq.GetPrintJobInfoCollection();
-
-                            foreach (var job in jobs)
-                            {
-                                var done = false;
-                                while (!done)
-                                {
-                                    pq.Refresh();
-                                    job.Refresh();
-                                    done =  job.IsDeleted ;
-                                }
-                                if (done)
-                                {
-                                    File.Delete(PathFilePrint);
-                                }
-                            }
-
-                            
-
-                        }
-                        catch (Exception ex)
-                        {
-                            MessageBox.Show(ex.Message);
-                        }
-
-
-                    }
                 }
+            }
+            if (File.Exists(PathFilePrint))
+            {
+                try
+                {
+                    DocumentName = Path.GetFileName(PathFilePrint);
+                    IPrinter printer = new Printer();
+                    printer.PrintRawFile(PrinterName, PathFilePrint, DocumentName);
+
+                    var myPrintServer = new LocalPrintServer();
+                    var pq = myPrintServer.GetPrintQueue(PrinterName);
+
+                    var jobs = pq.GetPrintJobInfoCollection();
+
+                    foreach (var job in jobs)
+                    {
+                        var done = false;
+                        while (!done)
+                        {
+                            pq.Refresh();
+                            job.Refresh();
+                            done = job.IsDeleted;
+                        }
+                        if (done)
+                        {
+                            File.Delete(PathFilePrint);
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+
             }
         }
     }
